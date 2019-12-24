fdafcd29df43aa30ffc01381544ef248ff3d5d76
Matthew Gizzi <matthewgizzi@verizon.net>
2019-12-24 21:26:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,7 +19,8 @@ def get_japanese_emoticon(file_path, emote)
   if dictionary[:get_emoticon][emote]
     dictionary[:get_emoticon][emote]
   else
-    puts 
+    puts "Sorry, that emoticon was not found"
+  end
 end
 
 def get_english_meaning
