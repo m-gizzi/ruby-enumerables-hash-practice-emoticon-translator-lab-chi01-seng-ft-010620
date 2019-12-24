e861276e51b89596a1ed4ecb504f7884638ab629
Matthew Gizzi <matthewgizzi@verizon.net>
2019-12-24 21:26:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,7 +16,10 @@ end
 
 def get_japanese_emoticon(file_path, emote)
   dictionary = load_library(file_path)
-  dictionary[:get_emoticon][emote]
+  if dictionary[:get_emoticon][emote]
+    dictionary[:get_emoticon][emote]
+  else
+    puts 
 end
 
 def get_english_meaning
