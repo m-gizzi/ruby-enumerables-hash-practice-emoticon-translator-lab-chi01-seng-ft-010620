fee6d6bda7bed0b55b83f8038213fa00cf854157
Matthew Gizzi <matthewgizzi@verizon.net>
2019-12-24 21:23:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,6 +11,7 @@ def load_library(file_path)
   english.each do | word |
     return_hash[:get_emoticon][start[word][0]] = start[word][1]
   end
+  return_hash
 end
 
 def get_japanese_emoticon(file_path, emote)
